cecb90d6ffd604b381e57e7868ed4df43ee6694f
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-02-11 11:58:39 +0100

    fix robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://veritate.at/sitemap.xml',
+    sitemap: 'https://app.pixelix.social/sitemap.xml',
   }
 }
